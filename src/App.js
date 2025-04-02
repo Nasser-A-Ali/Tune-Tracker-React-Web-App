748510d26be943b88191ef4b12cb169ad4e3776b
Adam Stevenson <160768532+Adam-S988@users.noreply.github.com>
2025-04-02 13:02:42 -0230

    Started the basics for each page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import Artists from "./components/artists";
+import Albums from "./components/albums";
+import Songs from "./components/songs";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Tune Tracker</h1>
+      <ul>
+        <li>
+          <Link to="/artists">Artists</Link>
+        </li>
+        <li>
+          <Link to="/albums">Albums</Link>
+        </li>
+        <li>
+          <Link to="/songs">Songs</Link>
+        </li>
+      </ul>
+
+      <Routes>
+        <Route path="/artists" element={<Artists />} />
+        <Route path="/albums" element={<Albums />} />
+        <Route path="/songs" element={<Songs />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/songs.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const songs = () => {
+  return (
+    <div>
+      <h1>Songs</h1>
+    </div>
+  );
+};
+
+export default songs;
