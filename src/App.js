9ab2b2485637a8a6e12792aecd972bac37e1ee8e
Nasser Ali <nasserali60@live.com>
2025-04-14 19:14:27 -0400

    feat/style: add home page and made logo in header link to it

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
+import Home from "./components/home";
+
 import Artists from "./components/artists";
 import Albums from "./components/albums";
 import Songs from "./components/songs";
@@ -9,7 +11,10 @@ function App() {
   return (
     <div>
       <div id="header">
-        <h1>🎧 Tune Tracker 🎧</h1>
+        <Link to="/" className="header-logo">
+          <h1>🎧 Tune Tracker 🎧</h1>
+        </Link>
+
         <ul id="NavBar">
           <li>
             <Link to="/artists">Artists</Link>
@@ -24,6 +29,7 @@ function App() {
       </div>
 
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/artists" element={<Artists />} />
         <Route path="/albums" element={<Albums />} />
         <Route path="/songs" element={<Songs />} />
