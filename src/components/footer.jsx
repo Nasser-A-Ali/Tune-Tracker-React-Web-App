b3b13e9bb914b23abdf123f8bc32704cb5334d22
SearchingSteve <155838264+SearchingSteve@users.noreply.github.com>
2025-04-15 12:55:56 -0230

    Update application name to Tune Tracker
    - Update package name in package.json
    - Update page title in index.html
    - Update app name in manifest.json
    - Update footer component

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <div id="footer">
-      <p>&copy; 2025 Tune Track Ltd. All rights reserved.</p>
+      <p>&copy; 2025 Tune Tracker. All rights reserved.</p>
     </div>
   );
 }
